c4b83f707e94ef7fe1efa05f7c83854854537c05
Programacio Eng <PREng@upc.edu>
2022-03-21 16:08:22 +0100

    Añadir fondo e imagen

--- Tutorial.py
+++ Tutorial.py
@@ -10,8 +10,8 @@ import sys
 # -----------
 # Constantes
 # -----------
-SCREEN_WIDTH = 1080
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
 # ------------------------------
 # Clases y Funciones utilizadas
 # ------------------------------
@@ -23,6 +23,14 @@ def main():
     # creamos la ventana y le indicamos un titulo:
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("tutorial pygame parte 2")
+    # cargamos el fondo y una imagen (se crea objetos "Surface")
+    fondo = pygame.image.load("fondo.jpg").convert()
+    tux = pygame.image.load("tux.png").convert_alpha()
+    # Indicamos la posicion de las "Surface" sobre la ventana
+    screen.blit(fondo, (0, 0))
+    screen.blit(tux, (550, 200))
+    # se muestran lo cambios en pantalla
+    pygame.display.flip()
     while True:
     # Posibles entradas del teclado y mouse
         for event in pygame.event.get():
