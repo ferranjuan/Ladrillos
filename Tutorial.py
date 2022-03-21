3358bd6f45d3fba558fd9eb3d3bee27b2f99f979
Programacio Eng <PREng@upc.edu>
2022-03-21 16:51:23 +0100

    Añadir movimiento en pantalla

--- Tutorial.py
+++ Tutorial.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Escrito por Daniel Fuentes B.
-# ---------------------------
-# Importacion de los módulos
-# ---------------------------
 import pygame
 from pygame.locals import *
 import sys
@@ -26,13 +22,35 @@ def main():
     # cargamos el fondo y una imagen (se crea objetos "Surface")
     fondo = pygame.image.load("fondo.jpg").convert()
     tux = pygame.image.load("tux.png").convert_alpha()
+    tux_pos_x = 550
+    tux_pos_y = 200
     # Indicamos la posicion de las "Surface" sobre la ventana
+    screen.blit(tux, (tux_pos_x, tux_pos_y))
     screen.blit(fondo, (0, 0))
-    screen.blit(tux, (550, 200))
     # se muestran lo cambios en pantalla
     pygame.display.flip()
+    dx = -1
+    dy = -1
+    # el bucle principal del juego
+    clock = pygame.time.Clock()
     while True:
-    # Posibles entradas del teclado y mouse
+        # Calculamos la nueva posición de tux
+        # que no alcance el borde de la pantalla
+        tux_pos_x = tux_pos_x - 1
+        tux_pos_y = tux_pos_y - 1
+        if tux_pos_x < 1:
+            tux_pos_x = SCREEN_WIDTH
+        if tux_pos_y < 1:
+            tux_pos_y = SCREEN_HEIGHT
+
+        # Redibujamos todos los elementos de la pantalla
+        screen.blit(fondo, (0, 0))
+        screen.blit(tux, (tux_pos_x, tux_pos_y))
+        # se muestran lo cambios en pantalla
+        pygame.display.flip()
+        clock.tick(150)
+
+        # Posibles entradas del teclado y mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
